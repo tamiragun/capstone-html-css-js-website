9f66bb910c0c7d97abbb6416072ed122be1c12df
Tamira <t.gunzburg@gmail.com>
2021-04-15 12:00:16 +0200

    Ability to save multiple itms for later

--- js/save-for-later.js
+++ js/save-for-later.js
@@ -4,30 +4,30 @@
 let registryButton = $('.registry-button');
 //Then, adding an event listener and function to them
 registryButton.click(test);
-
-//Defining the function that will dave the description of the selected item into local storage
-function test(){
-    //First, selecting an element to be saved to LoccalStorage. 
-    let chosenObjectText = $(this).siblings('.registry-item-description');
-    let chosenObjectImage = $(this).siblings('.registry-image');
-    //Testing by printing
-    console.log(chosenObjectText.html());
-    console.log(chosenObjectImage.attr('src'));
-    //storing this data into an Item object
-    let chosenObjectItem = new Item(chosenObjectText.html(),chosenObjectImage.attr('src'));
-    //Printing this new Item object to test
-    console.log(chosenObjectItem);   
-    //Storing the selected object
-    localStorage.setItem("chosenObject", JSON.stringify(chosenObjectItem));
-    //Testing by printing it out:
-    console.log(chosenObjectItem);
-};
-
+//Declaring an empty string to store saved objects in
+let savedObjects = [];
 //Instantiating an object for saved items:
 function Item(description, imageSource) {
     this.description = description;
     this.imageSource = imageSource;
 }
+
+
+//Defining the function that will save the description of the selected item into local storage
+function test(){
+    //Getting the stored list of objects
+    savedObjects = JSON.parse(localStorage.getItem("items"));
+    //Identifying elements to be saved to localStorage. 
+    let chosenObjectText = $(this).siblings('.registry-item-description');
+    let chosenObjectImage = $(this).siblings('.registry-image');
+    //storing this data into an Item object
+    let chosenObjectItem = new Item(chosenObjectText.html(),chosenObjectImage.attr('src'));
+    //Adding the item to the array of saved items
+    savedObjects.push(chosenObjectItem);
+    //Storing the array back into local storage
+    localStorage.setItem("items", JSON.stringify(savedObjects));
+};
+
 
 
 //Creating a like-unlike button for each registry item:
--- js/saved-for-later.js
+++ js/saved-for-later.js
@@ -1,22 +1,20 @@
 //Displaying the saved objects on a new page
 
-//Now retrieving this item from local storge AND converting from Json back into javascript:
-let retrievedObject = JSON.parse(localStorage.getItem("chosenObject"));
+//Retrieving this list from local storge AND converting from Json back into javascript:
+let savedObjects = JSON.parse(localStorage.getItem("items"));
 
-//printing to test. It looks different now from when I printed the original object
-console.log(retrievedObject);
-
-//Creating a new element where I can display this saved object in:
-let newElement = document.createElement("div");
-//Assigning it the retrieved and parsed object:
-newElement.innerHTML = `<div class="registry-item" style="float:left; text-align:center">
-                <img class="registry-image" src="${retrievedObject.imageSource}" alt="" width="300" height="300">
-                <div class="registry-item-description">${retrievedObject.description}</div>
-            </div>`;
-//Printing to help debug:
-console.log(newElement);
-//Now trying to display this object on the other page. 
-document.getElementById('saved-items').appendChild(newElement);
+//Looping through the list of saved objects, and for each saved object displayingit on this page:
+savedObjects.forEach(function(p) {//Loop through each person (p) in the pers array
+    //Creating a new element where I can display this saved object in:
+    let newElement = document.createElement("div");
+    //Assigning it the desired HTML eith the retrieved and parsed object attributes:
+    newElement.innerHTML = `<div class="registry-item" style="float:left; text-align:center">
+                    <img class="registry-image" src="${p.imageSource}" alt="" width="300" height="300">
+                    <div class="registry-item-description">${p.description}</div>
+                </div>`;
+    //Displaying this object on the page:
+    document.getElementById('saved-items').appendChild(newElement);
+});
 
 
 
